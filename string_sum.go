9d52cd73d620f59f0dca15f8090815119d5db410
Dmitriy.Kravets <Dmitriy.Kravets@wolterskluwer.com>
2022-05-31 13:54:57 +0300

    checking the solution

--- string_sum.go
+++ string_sum.go
@@ -1,9 +1,10 @@
-package string_sum
+package main
 
 import (
 	"errors"
 	"fmt"
 	"strconv"
+	"strings"
 	"unicode"
 )
 
@@ -25,15 +26,15 @@ var (
 //
 // Use the errors defined above as described, again wrapping into fmt.Errorf
 
-// func main() {
-// 	st := "1+1"
-// 	s, e := StringSum(st)
-// 	fmt.Println(s, e)
-// }
+func main() {
+	st := "1-1"
+	s, e := StringSum(st)
+	fmt.Println(s, e)
+}
 
 func StringSum(input string) (output string, err error) {
 
-	firstAddenString, secondAddenString := "", ""
+	firstOperandString, secondOperandString := "", ""
 
 	// clear from whitespaces:
 	out := []rune(input)
@@ -43,38 +44,59 @@ func StringSum(input string) (output string, err error) {
 			newOut = append(newOut, out[k])
 		}
 	}
+
+	// check ig input is empty:
 	if len(newOut) == 0 {
 		return "", fmt.Errorf("%w", errorEmptyInput)
 	}
+
+	// check if first operand is negative
+	if newOut[0] != '-' {
+		newOut = append([]rune{'+'}, newOut...)
+	}
+
+	// preparing input string for operand number validation:
+
+	tmpStr := strings.ReplaceAll(string(newOut), "+", ",")
+	tmpStr = strings.ReplaceAll(string(tmpStr), "-", ",")
+
+	// Now when we split input string shuld get three item slice
+	// to be shure that the number of operands is correct
+	if len(strings.Split(tmpStr, ",")) != 3 {
+		return "", fmt.Errorf("incorrect number of operands: %w", errorNotTwoOperands)
+	}
+
+	// Now we are sure that number of operands is two.
+	// We are preparing operands:
 	for i := len(newOut) - 1; i >= 0; i-- {
 		if !unicode.IsDigit(rune(newOut[i])) {
-			firstAddenString = string(newOut[:i])
-			secondAddenString = string(newOut[i:])
+			firstOperandString = string(newOut[:i])
+			secondOperandString = string(newOut[i:])
 			break
 		}
 	}
 
 	// checking if operands number is greater than 2(skipping first rune because it is a sign):
-	for j := len(secondAddenString) - 1; j >= 1; j-- {
-		_, err := strconv.Atoi(string(secondAddenString[j]))
+	for j := len(secondOperandString) - 1; j >= 1; j-- {
+		_, err := strconv.Atoi(string(secondOperandString[j]))
 		if err != nil {
 			return "", fmt.Errorf("error in second operand: %w", err)
 		}
 	}
 
 	// checking if firs and second iperands are not empty strings
-	if len(firstAddenString) == 0 || len(secondAddenString) == 0 {
+	if len(firstOperandString) == 0 || len(secondOperandString) == 0 {
 		return "", fmt.Errorf("incorrect number of operands: %w", errorNotTwoOperands)
 	}
 
 	// checking if the first operand has letters by converting it to integer
-	firstItem, err := strconv.ParseInt(string(firstAddenString), 10, 64)
+	firstItem, err := strconv.ParseInt(string(firstOperandString), 10, 64)
 	if err != nil {
 		return "", fmt.Errorf("error in first operand: %w", err)
 	}
 
 	// checking if second operand has letters by converting it to integer
-	secItem, err := strconv.ParseInt(string(secondAddenString), 10, 64)
+	secItem, err := strconv.ParseInt(string(secondOperandString), 10, 64)
 	if err != nil {
 		err = fmt.Errorf("error in second operand: %w", err)
 		return "", err
